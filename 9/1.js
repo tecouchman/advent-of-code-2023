90e4acc30976ce0ed6e4a94cbbdceb75e7fa1a97
Tom Couchman <tecouchman@gmail.com>
2023-12-09 22:02:11 +0900

    Day 9

---
+++ 9/1.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+let input = fs.readFileSync(path.join(__dirname, `../input/${path.basename(__dirname)}/input.txt`), 'utf8').split('\n');
+
+let total = 0;
+let parseNumbers = (str) => str.split(' ').filter((num) => num != '').map((num) => Number(num));
+let lines = input.filter(str => str != '').map(str => [ parseNumbers(str) ]);
+
+lines.forEach(line => {
+    let n = 0;
+
+    while (line[n].some((num, index) => num != 0)) {
+        let next = [];
+        for (let i = 1; i < line[n].length; i++) {
+            next.push(line[n][i] - line[n][i-1]);
+        }
+        line.push(next);
+        n++;
+    }
+    console.log(line);
+
+    total += line.reduce((acc, curr) => acc += curr[curr.length - 1], 0);
+});
+
+console.log(total);
---
+++ 9/2.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+let input = fs.readFileSync(path.join(__dirname, `../input/${path.basename(__dirname)}/input.txt`), 'utf8').split('\n');
+
+let total = 0;
+let parseNumbers = (str) => str.split(' ').filter((num) => num != '').map((num) => Number(num));
+let lines = input.filter(str => str != '').map(str => [ parseNumbers(str) ]);
+
+lines.forEach(line => {
+    let n = 0;
+
+    while (line[n].some((num, index) => num != 0)) {
+        let next = [];
+        for (let i = 1; i < line[n].length; i++) {
+            next.push(line[n][i] - line[n][i-1]);
+        }
+        line.push(next);
+        n++;
+    }
+
+    let newVal = line[line.length - 2][0];
+    for (let i = line.length - 2; i > 0; i--) {
+        newVal = line[i-1][0]-newVal;
+    }
+    total += newVal;
+});
+
+console.log(total);
